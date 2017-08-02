d86418e555a60ddd6f4b2d1d94ecfdb493e078cb
Renato Rodrigues <renatorodriguesnet@gmail.com>
2017-08-02 17:32:30 -0300

    Removing zuul route

--- src/main/java/br/com/snackbar/server/SnackBarServerApplication.java
+++ src/main/java/br/com/snackbar/server/SnackBarServerApplication.java
@@ -3,14 +3,12 @@ package br.com.snackbar.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @EnableCaching
 @SpringBootApplication
 public class SnackBarServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(SnackBarServerApplication.class, args);
 	}
 }
