4059fbc2f339681cdfef648b6ef42bf957f87e34
Renato Rodrigues <renatorodriguesnet@gmail.com>
2017-08-03 23:16:17 -0300

    Exposing price calculator service

--- src/main/java/br/com/snackbar/server/controller/ProductController.java
+++ src/main/java/br/com/snackbar/server/controller/ProductController.java
@@ -1,13 +1,17 @@
 package br.com.snackbar.server.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.snackbar.server.dto.ProductItemDTO;
 import br.com.snackbar.server.model.Product;
 import br.com.snackbar.server.service.ProductService;
 
@@ -24,6 +28,16 @@ public class ProductController {
 		List<Product> products = this.productService.list();
 
 		return ResponseEntity.ok(products);
+
+	}
+
+	@PostMapping("calculatePrice")
+	public ResponseEntity<BigDecimal> calculatePrice(@RequestBody final List<ProductItemDTO> items) {
+
+		BigDecimal price = this.productService.calculatePrice(items);
+
+		return ResponseEntity.ok(price);
+
 	}
 
 }
